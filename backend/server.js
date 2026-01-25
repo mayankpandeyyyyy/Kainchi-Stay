e8a242c3402b98a5e4bd60f8be66e274fc22aab9
mayankpandeyyyyy <mayankpandeyyyyy1705@gmail.com>
2026-01-25 21:00:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 // 6. START SERVER
 // Binding to 0.0.0.0 is essential for Render deployment
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server is flying on port ${PORT}`);
 });
